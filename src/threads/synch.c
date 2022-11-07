3e202c5067b9be1bc0fe4078b3a9022a23708997
samith <samithkaruanathilake@gmail.com>
2022-11-08 04:50:26 +0530

    synch.c changed according to Priority

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -66,10 +66,12 @@ sema_down (struct semaphore *sema)
   ASSERT (!intr_context ());
 
   old_level = intr_disable ();
+  void* aux;
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
-      thread_block ();
+//        list_push_back (&sema->waiters, &thread_current ()->elem);
+        list_insert_ordered(&sema->waiters, &thread_current()->elem,&thread_max_priority,aux);
+        thread_block ();
     }
   sema->value--;
   intr_set_level (old_level);
@@ -295,7 +297,9 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+    void *aux;
+    list_insert_ordered(&cond->waiters, &waiter.elem,&thread_max_priority,aux);
+//  list_push_back (&cond->waiters, &waiter.elem);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
